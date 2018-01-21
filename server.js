5f4186a44460c4e45bded091e5af3e2790f00245
Dmytrakov Serhii <admin@192.168.0.102.local.ipnet.ua>
2018-01-21 18:47:08 +0200

    Add output subscribers from DB on route /users

--- server.js
+++ server.js
@@ -2,21 +2,17 @@ require('./models/subscriber.model')
 const bodyParser = require('body-parser')
 const config = require('./config')
 const express = require('express')
-// const fs = require('fs')
 const mongoose = require('mongoose')
 const path = require('path')
 const qs = require('querystring')
 const Subscriber = mongoose.model('subscribers')
 const validateSubscriptionForm = require('./public/common/validation')
-// const waitInterval = 100000
 
-mongoose.connect(config.DB_URL, {
-  useMongoClient: true
-}).then(() => console.log('MongoDB connected')).catch((err) => console.log(err))
+mongoose.connect(config.DB_URL)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err))
 
 const app = express()
-
-// let db = JSON.parse(fs.readFileSync('db.json', 'utf-8'))
 
 app.use('/', express.static(path.join(__dirname, './public')))
 
@@ -26,14 +22,16 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-// app.get('/users', (req, res, next) => {
-//   let html = "<h3>Подписчики</h3><ol>"
-//   for (let user of db.users) {
-//     html = html + "<li>" + user.username + " - " + user.email + "</li>"
-//   }
-//   html += "</ol>"
-//   res.send(html)
-// })
+app.get('/users', (req, res, next) => {
+  Subscriber.find((err, subscribers) => {
+    let html = "<h3>Подписчики</h3><ol>"
+    for (let user of subscribers) {
+      html = html + "<li>" + user.username + " - " + user.email + "</li>"
+    }
+    html += "</ol>"
+    res.send(html)
+  })
+})
 
 app.post('/subscribe', (req, res) => {
   let username = req.body.username
@@ -41,7 +39,6 @@ app.post('/subscribe', (req, res) => {
   let result = validateSubscriptionForm({username, email})
   res.writeHead(200, {'Content-Type': 'application/json'})
   if (result) {
-    // db.users.push({username: req.body.username, email: req.body.email, status: 'subscribed'})
     new Subscriber({ username: username, email: email, status: "subscribed" })
       .save().then(() => console.log('Save successfully'))
 
